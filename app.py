3e2b0242e2c3a7f45b7e8eb9cdde845db4607112
Veer Sandhu <sandhu.s.veer@gmail.com>
2021-09-30 22:41:34 -0400

    nav bar

--- app.py
+++ app.py
@@ -11,14 +11,14 @@ st.set_page_config(page_title="Article-AI", page_icon="📰", layout='centered',
 
 st.markdown("""
 # Article AI
-Use Natural Language Processing algorithms to obtain articles and **produce brief summaries**
+Use Natural Language Processing to obtain articles and **produce brief summaries**
 """)
 
-st.markdown('<button style="border-color: black; border-radius: 10;" ><a style="-webkit-appearance: button; -moz-appearance: button; appearance: button; text-decoration: none; color: initial;" href="https://diplomatica.vercel.app/dashboard" target="_blank">Return to Dashboard</a></button>', unsafe_allow_html=True)
+st.markdown('----')
 
 # Other pages
-app.add_app("Search Term", web_search.app)
-app.add_app("URL", summarize.app)
+app.add_app("Via Search Term", web_search.app)
+app.add_app("Via URL", summarize.app)
 
 # The main app
 app.run()
--- multiapp.py
+++ multiapp.py
@@ -40,7 +40,7 @@ class MultiApp:
 
     def run(self):
         # app = st.sidebar.radio(
-        app = st.selectbox(
+        app = st.sidebar.selectbox(
             'Navigation',
             self.apps,
             format_func=lambda app: app['title'])
