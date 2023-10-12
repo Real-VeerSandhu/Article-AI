76672f95009b876a749acd8bb62e868ad94e6e2f
Veer Sandhu <sandhu.s.veer@gmail.com>
2023-10-11 23:51:11 -0400

    tab

--- components/web_summarize.py
+++ components/web_summarize.py
@@ -51,7 +51,7 @@ def get_links(term):
 
     for i in range(10):
         articles.append(df['articles'][i]['url'])
-    
+
     return articles 
 
 def fetch_text_data(input_url): # Fetch article data
